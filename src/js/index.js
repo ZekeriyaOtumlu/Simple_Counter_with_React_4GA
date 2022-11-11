38e39599deb6e582a287fcb2fac8796318944d4f
ZekeriyaOtumlu <zekyotumlu@hotmail.com>
2022-11-11 17:33:39 +0000

    Completed project

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,23 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+setInterval(() => {
+    seconds = seconds+1;
+    if(seconds==60){
+        seconds=0;
+        minutes = minutes+1;
+    }
+    if(minutes == 60){
+        minutes=0;
+        hours = hours+1;
+    }
+    ReactDOM.render(<Home hours ={hours} minutes={minutes} seconds={seconds} />, document.getElementById("app"));
+  }, 10);
+  
+
